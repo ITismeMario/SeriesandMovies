8530583abec43464e9e804702e3d23ef054f70e7
Mario A. Rodríguez <marm218@outlook.com>
2021-07-30 11:00:51 -0500

    The html markup was modified for visual purposes

--- js/views/overviewView.js
+++ js/views/overviewView.js
@@ -22,56 +22,34 @@ class OverviewView extends View {
 	_generateMarkup() {
 		//prettier-ignore
 		return `          
-        <div class="col-lg-4 image-container">
-            <div class="poster"><img src="${this._data.imageUrl}" alt="" /></div>
-        </div>
-        <div class="col-lg-8 summary-container">
-            <div class="display-title">
-                <h3 class="display-name">${this._data.title}</h3>
-                <p class="display-year">${this._data.year}</p>
-                <p class="display-genres">${this._data.genres.join(', ')}</p>
-            </div>
-            <div class="summary">
-                <h6>SUMMARY</h6>
-                <div class="line1"></div>
-                <p class="description">
-                  ${this._data.summary}
-                </p>
-                <div class="display-footer">
-                    <figure class="display-fig">
-                        <i class="fas fa-star rating-icon"></i>
-                    </figure>
+		    <div class="col-lg-4 image-container">
+				<div class="poster"><img src="${this._data.imageUrl}" alt="" /></div>
+			</div>
+			<div class="col-lg-8 summary-container">
+				<div class="display-title">
+					<h3 class="display-name">${this._data.title}</h3>
+					<p class="display-year">${this._data.year}</p>
+					<p class="display-genres">${this._data.genres.join(', ')}</p>
+				</div>
+				<div class="summary">
+					<h6>SUMMARY</h6>
+					<div class="line1"></div>
+					<p class="description">${this._data.summary}</p>
+					<div class="display-footer">
+						<figure class="display-fig">
+							<i class="fas fa-star rating-icon"></i>
+						</figure>
 
-                    <div class="display-score">${this._data.rating}</div>
-                </div>
+						<div class="display-score">${this._data.rating}</div>
+					</div>
 
+					<!-- THE ACTORS LIST WILL GO HERE -->
+
+				</div>
                 <div class="display-cast-header">PERFORMING</div>
-                <div class="line2"></div>
-                <div class="display-cast">
-                                   
-                <!-- THE ACTORS LIST GOES HERE -->
-
-                </div>
-            </div>
-        </div>
+				<div class="line2"></div>
+			</div>            
                 `;
-	}
-
-	/**
-	 * Adds the actors info to the document (requires the overview to be loaded already)
-	 */
-	generateActorsMarkup() {
-		const actorsList = this._data.principals;
-		const htmlCast = document.querySelector('.display-cast');
-
-		//prettier-ignore
-		return actorsList.map(actor => htmlCast.insertAdjacentHTML('afterbegin',         
-        `
-        <div class="display-actors">
-            <div class="display-performer">${actor[0]}</div>
-            <div class="display-role">as ${actor[1].join(', ')}</div>
-        </div>
-        `));
 	}
 }
 
