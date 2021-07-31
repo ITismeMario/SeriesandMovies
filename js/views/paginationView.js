3fd3cacaf65d29421027e449ff9d0c1f9612e735
Mario A. Rodríguez <marm218@outlook.com>
2021-07-31 12:13:16 -0500

    Fixed the buttons html tag to appear in the right order

--- js/views/paginationView.js
+++ js/views/paginationView.js
@@ -15,46 +15,46 @@ class PaginationView extends View {
 	}
 
 	_generateMarkup() {
-		const currPage = this._data.currentPage; //_data = model.state
+		const currPage = this._data.currentPage; //_data = model.state.currentPage
 		const numPages = Math.ceil(this._data.images.length / this._data.imagesPerPage);
 
 		//Page 1 and there are more pages.
 		if (currPage === 1 && numPages > 1) {
 			//prettier-ignore
 			return `
-            <button data-goto="${currPage + 1}" class="btn-inline pagination-btn-next">            
-                <i class="fas fa-angle-left arrow-icon"></i>
-                <span>${currPage + 1}</span>
-            </button> 
             <div class="current-page">${currPage}</div>
-        `;
+            <button data-goto="${currPage + 1}" class="btn-inline pagination-btn-next">                            
+                <span>${currPage + 1}</span>
+                <i class="fas fa-angle-right arrow-icon"></i>
+            </button> 
+            `;
 		}
 
 		//Last page
 		if (currPage === numPages && numPages > 1) {
 			//prettier-ignore
-			return `       
-            <div class="current-page">${currPage}</div>
+			return `                   
             <button data-goto="${currPage - 1}" class="btn-inline pagination-btn-prev">
-                <span>${currPage - 1}</span>
-                <i class="fas fa-angle-right arrow-icon"></i>
+                <i class="fas fa-angle-left arrow-icon"></i>    
+                <span>${currPage - 1}</span>                
             </button>
-        `;
+            <div class="current-page">${currPage}</div>
+            `;
 		}
 
 		//other page
 		if (currPage < numPages) {
 			//prettier-ignore
-			return `
-            <button data-goto="${currPage + 1}" class="btn-inline pagination-btn-next">            
-                <i class="fas fa-angle-left arrow-icon"></i>
-                <span>${currPage + 1}</span>
-            </button> 
-            <div class="current-page">${currPage}</div>
+			return `            
             <button data-goto="${currPage - 1}" class="btn-inline pagination-btn-prev">
+                <i class="fas fa-angle-left arrow-icon"></i>
                 <span>${currPage - 1}</span>
-                <i class="fas fa-angle-right arrow-icon"></i>
             </button>
+            <div class="current-page">${currPage}</div>
+            <button data-goto="${currPage + 1}" class="btn-inline pagination-btn-next">                        
+                <span>${currPage + 1}</span>
+                <i class="fas fa-angle-right arrow-icon"></i>
+            </button> 
       `;
 		}
 
