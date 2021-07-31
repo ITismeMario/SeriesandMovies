ee185425e917caf459bf3ccf366cf625cac4c904
Mario A. Rodríguez <marm218@outlook.com>
2021-07-30 21:28:16 -0500

    Created the this new view to render the buttons with the right page number they are taking you to, THIS FEATURE IS NOT TESTED YET

---
+++ js/views/paginationView.js
@@ -0,0 +1,66 @@
+import View from './View.js';
+
+class PaginationView extends View {
+	_parentElement = document.querySelector('.pagination');
+
+	addHandlerClick(handler) {
+		this._parentElement.addEventListener('click', function (e) {
+			const btn = e.target.closest('.btn-inline');
+
+			if (!btn) return;
+
+			const goToPage = +btn.dataset.goto;
+			handler(goToPage);
+		});
+	}
+
+	_generateMarkup() {
+		const currPage = this._data.currentPage; //_data = model.state
+		const numPages = Math.ceil(this._data.images.length / this._data.imagesPerPage);
+
+		//Page 1 and there are more pages.
+		if (currPage === 1 && numPages > 1) {
+			//prettier-ignore
+			return `
+            <button data-goto="${currPage + 1}" class="btn-inline pagination-btn-next">            
+                <i class="fas fa-angle-left arrow-icon"></i>
+                <span>${currPage + 1}</span>
+            </button> 
+            <div class="current-page">${currPage}</div>
+        `;
+		}
+
+		//Last page
+		if (currPage === numPages && numPages > 1) {
+			//prettier-ignore
+			return `       
+            <div class="current-page">${currPage}</div>
+            <button data-goto="${currPage - 1}" class="btn-inline pagination-btn-prev">
+                <span>${currPage - 1}</span>
+                <i class="fas fa-angle-right arrow-icon"></i>
+            </button>
+        `;
+		}
+
+		//other page
+		if (currPage < numPages) {
+			//prettier-ignore
+			return `
+            <button data-goto="${currPage + 1}" class="btn-inline pagination-btn-next">            
+                <i class="fas fa-angle-left arrow-icon"></i>
+                <span>${currPage + 1}</span>
+            </button> 
+            <div class="current-page">${currPage}</div>
+            <button data-goto="${currPage - 1}" class="btn-inline pagination-btn-prev">
+                <span>${currPage - 1}</span>
+                <i class="fas fa-angle-right arrow-icon"></i>
+            </button>
+      `;
+		}
+
+		//Page 1 and there are NO more pages
+		return '';
+	}
+}
+
+export default new PaginationView();
